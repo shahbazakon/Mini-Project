a0571106af3f103d3cc5531e1aee2d46c2a81d97
Mohd Shahbaz <57652434+shahbazakon@users.noreply.github.com>
2020-12-23 23:38:06 +0530

    WhatsApp Automate
    This code can automate WhatsApp and send message  bomb (which is set by you what is the message ? and number of messages )

---
+++ WhatsAppMsg.py
@@ -0,0 +1,18 @@
+import pyautogui
+import webbrowser as wb
+import time
+wb.open("https://web.whatsapp.com/")
+time.sleep(30)
+for i in range(5):
+    pyautogui.press("H")
+    pyautogui.press("e")
+    pyautogui.press("l")
+    pyautogui.press("l")
+    pyautogui.press("o")
+    pyautogui.press("enter")
+    pyautogui.press("w")
+    pyautogui.press("o")
+    pyautogui.press("r")
+    pyautogui.press("l")
+    pyautogui.press("d")
+    pyautogui.press("enter")
